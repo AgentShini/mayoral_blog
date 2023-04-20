792644b9a2397cd9f4e59057b6c89bf75f9f464a
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-04-21 00:41:13 +0100

    First commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv = "Content-Type" content = "text/html"; charset = "utf-8"/>
+    <meta name="author" content="Eightsoft">
+    <meta name = "description" content = "All about the AI"/>
+
+    <title>Home - Eightsoft blog</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    
+    <!-- Navigation bar (header) -->
+   @include('home.navbar')
+
+    <!-- Content body -->
+    <div class="wrapper">
+        <div class="container"><div class="row">
+
+            <!-- Images + Photo -->
+
+            <div class="col-lg-12">     <div class="row">
+
+                <div class="col-lg-12">
+                    <div class="row backgroundImage">
+                        <div class="col-lg-6">
+                             <div class="contentBlock">
+                                 <h6>ABOUT</h6>
+                                 <h1>Bringing the benefits of AI to everyone</h1>
+                                 <p>We provide a platform for thousands of people to exchange ideas and to 
+                                     expand our understanding of data science. While we allow
+                                      authors to publish articles,
+                                       we do not endorse each author’s contribution.</p>
+                             </div>
+                         </div>
+    
+                        <div class="col-lg-6"></div>
+                    </div>
+                </div>
+                
+
+<!--                 <div class="col-sm-6 col-md-4">     <div class="thumbnail">
+
+                    <img src="assets/img/1.jpg" alt="Title">
+
+                    <div class="caption">
+                        <h3>Title</h3>
+                        <p> Description</p>
+                        <p><a href="#" class="btn btn-primary" role="button">Like</a> 
+                            <a href="#" class="btn btn-primary" role="button">Comment</a>
+                            <a href="#" class="btn btn-primary" role="button">Read more</a>
+                        </p>
+                    </div>
+
+                </div></div> -->
+
+                <div class="col-sm-6 col-md-4">     <div class="thumbnail">
+
+                    <img src="assets/img/eclass.png" alt="Title">
+
+                    <div class="caption">
+                        <h3>Title</h3>
+                        <p> Description</p>
+                        <p><a href="#" class="btn btn-primary" role="button">Like</a> 
+                            <a href="#" class="btn btn-primary" role="button">Comment</a>
+                            <a href="#" class="btn btn-primary" role="button">Read more</a>
+                        </p>
+                    </div>
+
+                </div></div>
+
+                <div class="col-sm-6 col-md-4">     <div class="thumbnail">
+
+                    <img src="assets/img/eclass.png" alt="Title">
+
+                    <div class="caption">
+                        <h3>Title</h3>
+                        <p>Description</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">Like</a> 
+                            <a href="#" class="btn btn-primary" role="button">Comment</a>
+                            <a href="#" class="btn btn-primary" role="button">Read more</a>
+                            <!-- <a href="#"> <img class = "icon" src="assets/img/clap.png" alt="like"> </a> -->
+                        </p>
+                    </div>
+
+                </div> </div>
+
+                <div class="col-sm-6 col-md-4">     <div class="thumbnail">
+
+                    <img src="assets/img/eclass.png" alt="Title">
+
+                    <div class="caption">
+                        <h3>Title</h3>
+                        <p> Description</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">Like</a> 
+                            <a href="#" class="btn btn-primary" role="button">Comment</a>
+                            <a href="#" class="btn btn-primary" role="button">Read more</a>
+                        </p>
+                    </div>
+
+                </div></div>
+
+            </div></div>
+            
+
+        </div></div>
+
+        <div class="container">
+            <div class="row">
+
+                <div class="jumbotron jumbotron-fluid aboutPost">
+                    <div class="jumbotron ">
+                        <h6>BLOG</h6>
+                        <h2>Read our blog</h2>
+                        <p>We provide a platform for thousands of people to exchange ideas and to 
+                            expand our understanding of data science. While we allow
+                             authors to publish articles,
+                              we do not endorse each author’s contribution.</p>
+                        <a class="btn btn-primary btn-lg" href="blog.html" role="button">Learn more</a>
+                      </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="jumbotron jumbotron-fluid aboutPost aboutPostLeft">
+                            <div class="jumbotron ">
+                                <h6>DASHBOARD</h6>
+                                <h2>Discover</h2>
+                                <p>We provide a platform for thousands of people to exchange ideas and to 
+                                    expand our understanding of data science. While we allow
+                                     authors to publish articles,
+                                      we do not endorse each author’s contribution.</p>
+                                <a class="btn btn-primary btn-lg" href="dashboard.html" role="button">Learn more</a>
+                              </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="jumbotron jumbotron-fluid aboutPost aboutPostRight">
+                            <div class="jumbotron ">
+                                <h6>CONTACTS</h6>
+                                <h2>Contact us</h2>
+                                <p>We provide a platform for thousands of people to exchange ideas and to 
+                                    expand our understanding of data science. While we allow
+                                     authors to publish articles,
+                                      we do not endorse each author’s contribution.</p>
+                                <a class="btn btn-primary btn-lg" href="contact.html" role="button">Learn more</a>
+                              </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        
+
+        
+
+        <!-- <div class="container">    <div class="panel panel-default">
+
+                <div class="panel-heading">Popular posts</div>
+              
+                <table class="table table-striped">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Title</th>
+                            <th class="text-center">Rating</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td class="col-lg-1 col-md-1 col-xs-2">
+                                <img class="img-responsive" src="assets/img/eclass.png" alt="Title">
+                            </td>
+                            <td class="vert-align"><a href="">1</a></td>
+                            <td class="text-center vert-align"><span class="badge">10.0</span></td>
+                        </tr>  
+                        <tr>
+                            <td class="col-lg-1 col-md-1 col-xs-2">
+                                <img class="img-responsive" src="assets/img/eclass.png" alt="Title">
+                            </td>
+                            <td class="vert-align"><a href="">2</a></td>
+                            <td class="text-center vert-align"><span class="badge">10.0</span></td>
+                        </tr> 
+                        <tr>
+                            <td class="col-lg-1 col-md-1 col-xs-2">
+                                <img class="img-responsive" src="assets/img/eclass.png" alt="Title">
+                            </td>
+                            <td class="vert-align"><a href="">3</a></td>
+                            <td class="text-center vert-align"><span class="badge">8.0</span></td>
+                        </tr>  
+                    </tbody>
+        
+        
+                  </table>
+
+        </div>     </div>  -->
+        
+
+        <div class="clear"></div>
+    </div>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container-fluid">
+            <p class = "text-center"><a href="#">Input my github or some shii</a></p>
+        </div>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/home/navbar.blade.php
@@ -0,0 +1,53 @@
+<div class="container-fluid">    <div class="row">
+
+    <nav role="navigation" class="navbar navbar-inverse">
+        <div class="container">
+            
+            <!-- Logo (name) -->
+            <div class="navbar-header header">
+
+                <div class="col-lg-12"><div class="container"><div class="row">
+
+                        <h1><a href="#">Eightsoft</a></h1>
+                        <p><a href="#">All about AI</a></p>
+
+                </div></div></div>
+                
+
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+            
+            </div>
+            
+            <!-- Navigation buttons -->
+            <div class="col-lg-4"></div>
+            <div class="col-lg-8">
+                <div id="navbarCollapse"class="collapse navbar-collapse">
+                    <ul class="nav nav-pills nav-justified">
+                        <li role="presentation" class="active"><a href="">Home</a></li>
+                        <li role="presentation"><a href="blog.html">Blog</a></li>
+                        <li role="presentation"><a href="dashboard.html">Dashboard</a></li>
+                        <li role="presentation"><a href="contact.html">Contacts</a></li>
+                        <li role="presentation"><a href="login.html">Login</a></li>
+                        <li role="presentation"><a href="register.html">Register</a></li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Name
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="login.html">Log out</a>
+                            </div>
+                        </li>
+                      </ul>
+                </div>
+            </div>
+            
+        </div>
+    </nav>
+
+</div></div>
